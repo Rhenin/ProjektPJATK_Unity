cd2a4fe709178f2a63608d771d9c39ca847d0565
DESKTOP-M97CU4Q\Grafika Multimedia <dawidkadziolka@gmail.com>
2018-07-06 09:42:46 +0200

    Added Prefarb of image slider interface

---
+++ ProjektPJATK/Assets/ImageSlider/Scripts/SwapScenes.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwapScenes : MonoBehaviour {
+    //Trzeba dodac do sceny EvenTrigger component 
+    public void SceneLoader(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+}
